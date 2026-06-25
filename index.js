6064ddd54f2db9201efe3050f7c76df187a5d475
Aman Sahu <amansahuxy@gmail.com>
2026-06-25 22:37:46 +0530

    Implement grading system with conditional statements

--- index.js
+++ index.js
@@ -8,6 +8,32 @@ calc.addEventListener("click",()=>{
     let obtain=Number(obtainInput.value);
     let total=Number(totalInput.value);
     let percent=(obtain*100)/total;
-     
+     rslt.innerText=percent;
+    
+   if (percent >= 90 && percent <= 100) {
+    grad.innerText = "A+";
+}
+else if (percent >= 80 && percent < 90) {
+    grad.innerText = "A";
+}
+else if (percent >= 70 && percent < 80) {
+    grad.innerText = "B+";
+}
+else if (percent >= 60 && percent < 70) {
+    grad.innerText = "B";
+}
+else if (percent >= 50 && percent < 60) {
+    grad.innerText = "C+";
+}
+else if (percent >= 40 && percent < 50) {
+    grad.innerText = "C";
+}
+else if (percent < 40) {
+    grad.innerText = "Fail";
+}
+else {
+    grad.innerText = "Error";
+}
+
 
 })
