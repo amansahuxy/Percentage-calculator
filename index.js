34a20189978104366d432da906a22a8264eda179
Aman Sahu <amansahuxy@gmail.com>
2026-06-25 22:11:30 +0530

    Add percentage calculation functionality

---
+++ index.js
@@ -0,0 +1,12 @@
+const obtainInput=document.getElementById("input-obtain");
+const totalInput=document.getElementById("input-total");
+const calc=document.getElementById("calculate");
+const rslt=document.getElementById("result");
+const grad=document.getElementById("grade");
+
+calc.addEventListener("click",()=>{
+    let obtain=Number(obtainInput.value);
+    let total=Number(totalInput.value);
+    let percent=(obtain*100)/total;
+    
+})
